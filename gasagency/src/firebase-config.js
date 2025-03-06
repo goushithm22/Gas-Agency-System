4cd560f3844d1b4e078e26eaa1c0fbe35b4af1ee
goushithm22 <goushithm@gmail.com>
2025-03-07 00:23:48 +0530

    Update firebase-config.js

--- gasagency/src/firebase-config.js
+++ gasagency/src/firebase-config.js
@@ -4,13 +4,14 @@ const { getFirestore } = require("firebase/firestore");
 
 // Firebase Configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyABR4ypqXLaLcE-tpJtTUwvBNaKB7Cc-3c",
-    authDomain: "gas-management-system-4870c.firebaseapp.com",
-    projectId: "gas-management-system-4870c",
-    storageBucket: "gas-management-system-4870c.appspot.com",
-    messagingSenderId: "749348180428",
-    appId: "1:749348180428:web:1b2f21c7c034e66d608d69"
+  apiKey: "REPLACE_WITH_YOUR_API_KEY",
+  authDomain: "REPLACE_WITH_YOUR_AUTH_DOMAIN",
+  projectId: "REPLACE_WITH_YOUR_PROJECT_ID",
+  storageBucket: "REPLACE_WITH_YOUR_STORAGE_BUCKET",
+  messagingSenderId: "REPLACE_WITH_YOUR_MESSAGING_SENDER_ID",
+  appId: "REPLACE_WITH_YOUR_APP_ID"
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
